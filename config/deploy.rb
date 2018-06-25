901bcd29c74ccbec3b2243398e1b010ad29974e7
FranZisko <jfelmejor@gmail.com>
2018-06-25 18:49:43 -0400

    hola

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "Nexuzx"
-set :repo_url, "git@github.com:FranZisko/Nexuzx.git"
+set :application, "Nexuz"
+set :repo_url, "git@github.com:FranZisko/Nexuz.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
- set :deploy_to, "/home/Nexuzx"
+ set :deploy_to, "/home/Nexuz"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
